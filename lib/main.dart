4d3f041fd3ad585de0197d71b7b1bde957601352
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 <atul.sharma@hammerheadtechnology.com>
2024-02-20 16:45:42 +0530

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kestral',
       navigatorKey: Utils.navigatorKey,
+        debugShowCheckedModeBanner : false,
       theme: ThemeData(
        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
